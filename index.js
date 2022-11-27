04bf346c615f58ae808221b4c136dece3c8541ca
unknown <pgelvezandrea@gmail.com>
2022-11-26 21:07:47 -0500

    arreglos

--- index.js
+++ index.js
@@ -26,3 +26,62 @@ Morris.Bar({
     },
   });
 hideHover: true;
+
+
+
+
+new Morris.Line({
+  // ID of the element in which to draw the chart.
+  element: 'myfirstchart',
+  // Chart data records -- each entry in this array corresponds to a point on
+  // the chart.
+  data: [
+    { year: '2018', value: 20 },
+    { year: '2019', value: 50 },
+    { year: '2020', value: 35 },
+    { year: '2021', value: 15 },
+    { year: '2022', value: 98 }
+  ],
+
+  labelColor: '#BEC4C7',
+  axes : false,
+  grid :false,
+
+  
+  
+  lineColors:[
+      '#19C0CC ',
+      '#FA9111',
+      
+      
+  ],
+  // The name of the data record attribute that contains x-values.
+  xkey: 'year',
+  // A list of names of data record attributes that contain y-values.
+  ykeys: ['value'],
+  // Labels for the ykeys -- will be displayed when you hover over the
+  // chart.
+  labels: ['Value']
+  
+});
+
+Morris.Donut({
+  element: 'Donut',
+  data: [
+    {value: 298, label: 'Occupied'},
+    {value: 249, label: 'Vacan'},
+    {value: 85, label: 'Maintenance'},
+  ],
+
+  
+
+  
+  labelColor: '#11383d',
+  colors: [
+    '#2253DD ',
+    '#FA9111',
+    '#19C0CC'
+    
+  ],
+  formatter: function (x) { return x + "%"}
+});
